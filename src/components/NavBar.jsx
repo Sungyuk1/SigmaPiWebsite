2d9d002199f749784c7b38fc64e4a5a90329fd9c
Sungyu Kwon <kwonsun7@msu.edu>
2022-10-03 12:25:00 -0400

    Will attempt to fix tailwind changes not showing up error

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+//Just lets you import icons
+import {FaBars, FaTimes} from 'react-icons/fa' 
 
 const NavBar = () => {
-  return (
-    <div>NavBar</div>
+    //fixed will fix the navigation bar on top so it will not move even if you scroll
+    //text-white makes the text in the h1 white
+    //bg-black makes the background black
+    //px - padding
+    //ml- margin left of blank
+    return (
+    <div className="flex justify-between items-center w-full h-20 text-white px-4
+    bg-black fixed">
+        <div>
+            <h1 className="text-5xl font-signature ml-2">Sungyu</h1>
+        </div>
+    </div>
   )
 }
 
