afe2336cd4bb39bd97460afe0566f1602f205fd5
Sungyu Kwon <kwonsun7@msu.edu>
2022-10-03 12:43:39 -0400

    Fixed Tailwind error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{js, jsx, ts, tsx, html}",
+  "./src/**/*"],
   theme: {
     extend: {},
     fontFamily : {
