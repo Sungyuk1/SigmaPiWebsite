a9f922b1d34573c283f140c482d2b4b6ff8ba620
Sungyu Kwon <kwonsun7@msu.edu>
2022-10-04 01:31:59 -0400

    1.20 mark in the video

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import SocialLinks from "./components/SocialLinks";
 import About from "./components/About";
+import Portfolio from "./components/Portfolio";
 
 function App() {
   return (
@@ -9,7 +10,9 @@ function App() {
       <NavBar/>
       <Home/>
       <About/>
+      <Portfolio/>
       <SocialLinks/>
+      
     </div>
   );
 }
