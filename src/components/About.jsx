481ff502f1f84563e4047fbc07eca00851aef5d2
Sungyu Kwon <kwonsun7@msu.edu>
2022-10-04 00:52:48 -0400

    Before the Portfolio part

---
+++ src/components/About.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+            <div className="pb-8">
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+            </div>
+            {/*mt means margin top*/}
+            <p className='text-xl mt-20'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis explicabo quod blanditiis sed, ullam nihil 
+                cum dolore modi vel provident. Facilis veniam autem, consequuntur cum quisquam id voluptatum cupiditate soluta blanditiis
+                 corrupti nihil non facere laudantium. Modi earum non recusandae voluptate dolor tempore 
+                 impedit debitis vitae totam dicta. Nesciunt, ut!
+            </p>
+
+            <br/>
+            <p className="text-xl">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus impedit iusto inventore quia maiores ipsam tempora, 
+                tempore eligendi asperiores! Qui iusto expedita quo rem dolorem, fuga est rerum deserunt, atque optio dolore quia et? 
+                Quaerat quis odio reprehenderit porro perspiciatis veritatis officiis ipsum? Repellat ipsam facere perferendis perspiciatis nam. Hic!
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
