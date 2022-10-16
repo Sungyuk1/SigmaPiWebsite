15f6e64f2e46b94f22c26ba69c645b926ccb8d57
Sungyu Kwon <kwonsun7@msu.edu>
2022-10-16 12:27:55 -0400

    1:24

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,12 +3,36 @@ import ease_logo from "../assets/ease logo-png.png"
 
 const Portfolio = () => {
 
-  /*const portfolios = {
+    //you can have links here, and have the portfolio go to the links if you add a onclick handler
+  const portfolios = [
     {
-        id: 1,
-        src: arrayDestruct
+        id:1,
+        src: ease_logo
     },
-  }*/
+    {
+        id:2,
+        src: ease_logo
+    },
+    {
+        id:3,
+        src: ease_logo
+    },
+    {
+        id:4,
+        src: ease_logo
+    },
+    {
+        id:5,
+        src: ease_logo
+    },
+    {
+        id:6,
+        src: ease_logo
+    },
+
+  ]
+
+    
 
   return (<div name="portfolio" className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
     {/*Name is required for the smoothe scroll*/}
@@ -21,16 +45,21 @@ const Portfolio = () => {
                 Check out some of my work right here
             </p>
         </div>
+
         <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
-            <div className="shadow-md shadow-gray-600 rounded-lg">
-                <img src={ease_logo} alt="" className="rounded-md duration-200 hover:scale-105" />
+
+        {
+            portfolios.map(({id, src}) => (
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+                <img src={src} alt="" className="rounded-md duration-200 hover:scale-105" />
                 <div className="flex items-center justify-center">
                     <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Demo</button>
                     <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Code</button>
                 </div>
             </div>
-
-        </div>
+            ))
+        }
+        </div>       
     </div>
 </div> )
 }
