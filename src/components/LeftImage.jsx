29287f6711a73e7c7bb526fd4ac40c7b9603d9bb
Sungyu Kwon <kwonsun7@msu.edu>
2023-04-29 15:07:08 -0400

    Items Center for Left and right

--- src/components/LeftImage.jsx
+++ src/components/LeftImage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function LeftImage({image, title, text, name}) {
   return (
     <div className='flex flex-row w-full bg-gradient-to-b from-black to-gray-800 text-white' name={name}>
-        <div className="max-w-screen-lg p-4 mx-auto flex flex-row justify-start w-full h-3/4">
+        <div className="max-w-screen-lg p-4 mx-auto flex flex-row justify-start w-full h-3/4 items-center">
         <img src={image} alt="" className='h-1/2 w-1/2 mr-5 object-scale-down'/>
         <div className='flex flex-col ml-5'>
                 <div className="pb-8">
