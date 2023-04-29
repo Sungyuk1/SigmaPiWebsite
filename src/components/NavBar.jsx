e15d3c30cc9e73e082b3e1e58592315b051877e8
Sungyu Kwon <kwonsun7@msu.edu>
2023-04-29 04:12:25 -0400

    Changing navbar on mobile to be smaller and have bottom border

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -61,9 +61,9 @@ const NavBar = () => {
 
        {/*This means show only is nav is true*/}
         {nav && (
-             <ul className = "flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+             <ul className = "flex flex-col justify-center items-center absolute top-0 left-0 w-full h-3/4 bg-gradient-to-b from-black to-gray-800 text-gray-500 border-b-4 border-gray-800">
              {links.map(({id, link})=>(
-                <li key={id} className="px-4 cursor-pointer capitalize py-6 text-2xl"><Link to={link} smooth duration ={500}>{link}</Link></li>
+                <li key={id} className="px-4 cursor-pointer capitalize py-3 text-2xl"><Link to={link} smooth duration ={500}>{link}</Link></li>
  
              ))}
          </ul>
