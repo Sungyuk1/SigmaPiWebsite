f026680ec1c20e8eaa1c9fff2c7b420935f60053
Sungyu Kwon <kwonsun7@msu.edu>
2023-04-29 15:09:56 -0400

    Items are Center

--- src/components/RightImage.jsx
+++ src/components/RightImage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function RightImage({image, text, title, name}) {
   return (
     <div className='flex flex-row w-full bg-gradient-to-b from-gray-800 to-black text-white' name={name}>
-        <div className="max-w-screen-lg p-4 mx-auto flex flex-row items-center justify-center w-full h-full">
+        <div className="max-w-screen-lg p-4 mx-auto flex flex-row items-center justify-end w-full h-full">
         <div className='flex flex-col mr-5'>
             <div className="pb-8">
                 <p className='text-xs md:text-4xl font-bold inline border-b-4 border-gray-500'>{title}</p>
