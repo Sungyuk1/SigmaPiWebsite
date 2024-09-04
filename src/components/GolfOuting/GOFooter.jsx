63dd05fbe4d7193861319960d1dfec6474f7de20
Tyson Lance <lancetys@msu.edu>
2024-09-04 10:53:44 -0400

    changing footer

--- src/components/GolfOuting/GOFooter.jsx
+++ src/components/GolfOuting/GOFooter.jsx
@@ -5,11 +5,11 @@ const GOFooter = () => {
     return (
         <footer className="w-full bg-purple-900 p-6 flex flex-col md:flex-row md:justify-between md:items-center">
           <div className="flex flex-col md:flex-row md:flex-wrap md:items-center text-white text-center md:text-left mb-4 md:mb-0 w-full md:w-auto">
-            <div className="w-full md:w-auto mb-4 md:mb-0 md:mr-8">
-              <p>Website Feedback: Reach out to <a href="mailto:tysonblance@gmail.com" className="underline">tysonblance@gmail.com</a></p>
-            </div>
             <div className="w-full md:w-auto">
               <p>Golf Outing Questions: Reach out to <a href="mailto:chadbeckeman@gmail.com" className="underline">chadbeckeman@gmail.com</a></p>
+            </div>
+            <div className="w-full md:w-auto mb-4 md:mb-0 md:mr-8">
+              <p>Website Feedback: Reach out to <a href="mailto:tysonblance@gmail.com" className="underline">tysonblance@gmail.com</a></p>
             </div>
           </div>
           <div className="flex justify-center md:justify-end w-full md:w-auto">
